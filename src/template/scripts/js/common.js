be03d17f4d70ab11ee8823eb0666df26641db04a
Julia <jv@bananazbrain.ru>
2021-07-14 13:36:36 +0300

    sripts update

--- src/template/scripts/js/common.js
+++ src/template/scripts/js/common.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-	
-})
+let $html = document.querySelector('html');
+let $body = document.querySelector('body');
+let $inner = document.querySelector('.inner');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
